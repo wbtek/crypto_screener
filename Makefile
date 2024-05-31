8bc143afd6ea70df0da0f1fad60a102a8a1af24f
Greg Slocum <fromgit@wbtek.slocum.net>
2024-06-01 03:03:48 +0900

    Better filtering, moved index.html

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 build:
 	wasm-pack build --target web
 
-install:
-	cp index.html web/
+install: build
+	cp static/index.html web/
 	cp pkg/crypto_screener.js web/pkg/
 	cp pkg/crypto_screener_bg.wasm web/pkg/
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,22 @@
-
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestInit, RequestMode, Response};
-// use serde::Deserialize;
+use web_sys::{Request, RequestInit, RequestMode, Response, Headers};
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
+use js_sys::JSON;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Coin {
+    id: String,
+    symbol: String,
+    name: String,
+    total_volume: f64,
 }
 
 #[wasm_bindgen]
@@ -16,8 +25,11 @@ pub async fn get_crypto_data(_symbol: &str) -> Result<JsValue, JsValue> {
     opts.method("GET");
     opts.mode(RequestMode::Cors);
 
-    let url = format!("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=10&page=1");
+    let url = format!("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=volume_desc&per_page=50&page=1");
     let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    let headers = Headers::new().unwrap();
+    headers.set("Accept", "application/json")?;
     request.headers().set("Accept", "application/json")?;
 
     let window = web_sys::window().unwrap();
@@ -25,7 +37,20 @@ pub async fn get_crypto_data(_symbol: &str) -> Result<JsValue, JsValue> {
     assert!(resp_value.is_instance_of::<Response>());
     let resp: Response = resp_value.dyn_into().unwrap();
     let json = JsFuture::from(resp.json()?).await?;
-    Ok(json)
+    
+    let json_string = JSON::stringify(&json).unwrap().as_string().unwrap();
+    let coins: Vec<Coin> = serde_json::from_str(&json_string).unwrap();
+
+    let filtered_coins: Vec<Coin> = coins.into_iter()
+        .filter(|coin| 
+            coin.symbol != "dai" &&
+            coin.symbol != "weth" && 
+            !coin.symbol.contains("usd") && 
+            !coin.name.contains("Wrapped") && 
+            !coin.name.contains("Stable") )
+        .collect();
+
+    Ok(to_value(&filtered_coins).unwrap())
 }
 
 #[wasm_bindgen]
